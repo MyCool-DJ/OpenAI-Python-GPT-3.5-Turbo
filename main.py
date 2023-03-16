d1c35c58dd97d3c89224338dc931464f499869d8
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2023-03-16 04:04:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import openai
+import os
+import sys
+  
+try:
+  openai.api_key = os.environ['OPENAI_API_KEY']
+except KeyError:
+  sys.stderr.write("""
+  You haven't set up your API key yet.
+  
+  If you don't have an API key yet, visit:
+  
+  https://platform.openai.com/signup
+
+  1. Make an account or sign in
+  2. Click "View API Keys" from the top right menu.
+  3. Click "Create new secret key"
+
+  Then, open the Secrets Tool and add OPENAI_API_KEY as a secret.
+  """)
+  exit(1)
+
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+
+print(response)
